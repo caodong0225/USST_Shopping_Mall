f2bf945999553b22ada6723ac5403889290a2fad
jyzxc <867137498@qq.com>
2024-11-15 00:24:59 +0800

    feat: add the gateway

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 https://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-8.0.11.zip
 
 然后在idea里面引入
+
+下载consul，然后使用
+
+```
+consul agent -dev
+```
+
+来启动consul
---
+++ gateway/src/main/java/caodong0225/gateway/GatewayApplication.java
@@ -0,0 +1,19 @@
+package caodong0225.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author jyzxc
+ * @since 2024-11-14
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}

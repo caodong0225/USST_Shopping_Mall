52b2a38016e0fa20f5d4a115dc1750c821a7030d
jyzxc <867137498@qq.com>
2024-11-14 12:41:49 +0800

    feat: add the login service

---
+++ login/src/main/java/caodong0225/login/LoginApplication.java
@@ -0,0 +1,17 @@
+package caodong0225.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author jyzxc
+ * @since 2024-11-14
+ */
+@SpringBootApplication
+public class LoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginApplication.class, args);
+    }
+
+}

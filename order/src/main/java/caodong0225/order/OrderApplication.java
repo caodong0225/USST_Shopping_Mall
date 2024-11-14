c8996c749b0c65ff0bba57f286cfc8b37a1b3ee8
jyzxc <867137498@qq.com>
2024-11-14 21:18:00 +0800

    feat: add microservice to consul

--- order/src/main/java/caodong0225/order/OrderApplication.java
+++ order/src/main/java/caodong0225/order/OrderApplication.java
@@ -2,12 +2,14 @@ package caodong0225.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author jyzxc
  * @since 2024-11-13
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class OrderApplication {
 
     public static void main(String[] args) {

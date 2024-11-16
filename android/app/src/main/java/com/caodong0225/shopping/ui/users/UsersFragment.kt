e14124d61bf6ad0c77130ac12c87074c8620dd27
曹东 <867137498@qq.com>
2024-11-16 22:37:30 +0800

    feat: add the network connection permission

--- android/app/src/main/java/com/caodong0225/shopping/ui/users/UsersFragment.kt
+++ android/app/src/main/java/com/caodong0225/shopping/ui/users/UsersFragment.kt
@@ -10,7 +10,6 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.caodong0225.shopping.R
 import com.caodong0225.shopping.databinding.FragmentUsersBinding
-import com.caodong0225.shopping.model.ApiResponse
 import com.caodong0225.shopping.model.UsersRequest
 import com.caodong0225.shopping.repository.UsersRepository
 import com.caodong0225.shopping.ui.register.RegisterActivity
@@ -18,6 +17,7 @@ import com.google.android.material.textfield.TextInputEditText
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class UsersFragment : Fragment() {
 
@@ -45,19 +45,22 @@ class UsersFragment : Fragment() {
         loginButton?.setOnClickListener {
             val username = usernameEditText?.text.toString()
             val password = passwordEditText?.text.toString()
+            println("username: $username, password: $password")
 
             CoroutineScope(Dispatchers.IO).launch {
                 val response = usersRepository.fetchJwtToken(UsersRequest(username, password))
-                if (response?.data != null) {
-                    // 登录成功
-                    Toast.makeText(context, "登录成功", Toast.LENGTH_SHORT).show()
-                } else {
-                    // 登录失败
-                    Toast.makeText(context, response?.message, Toast.LENGTH_SHORT).show()
+                println("response: $response")
+                withContext(Dispatchers.Main) {
+                    val message = response?.message?.takeIf { it.isNotEmpty() } ?: "登录失败"
+                    if (response?.data != null) {
+                        // 登录成功
+                        Toast.makeText(context, "登录成功", Toast.LENGTH_SHORT).show()
+                    } else {
+                        // 登录失败
+                        Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+                    }
                 }
             }
-
-            Toast.makeText(context, "用户名: $username\n密码: $password", Toast.LENGTH_SHORT).show()
         }
 
         // 注册按钮跳转到注册页面

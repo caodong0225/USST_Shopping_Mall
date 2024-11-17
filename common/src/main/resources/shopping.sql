1bcace3144fea34874e46581665544af25a8a261
曹东 <867137498@qq.com>
2024-11-17 11:34:45 +0800

    chore: update the sql structure

--- common/src/main/resources/shopping.sql
+++ common/src/main/resources/shopping.sql
@@ -11,41 +11,72 @@
  Target Server Version : 80038 (8.0.38)
  File Encoding         : 65001
 
- Date: 14/11/2024 15:27:21
+ Date: 17/11/2024 11:34:03
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
+-- Table structure for goods
+-- ----------------------------
+DROP TABLE IF EXISTS `goods`;
+CREATE TABLE `goods`  (
+                          `id` int NOT NULL AUTO_INCREMENT COMMENT '主键',
+                          `name` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '商品名称',
+                          `status` int NOT NULL DEFAULT 1 COMMENT '商品状态（1正在售卖，2已卖光，3商品已下架）',
+                          `price` decimal(10, 2) NOT NULL DEFAULT 0.00 COMMENT '商品单价',
+                          `logo` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL COMMENT '商品图标的地址',
+                          `stock` int NOT NULL DEFAULT 0 COMMENT '库存',
+                          PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '商品表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for order_detail
+-- ----------------------------
+DROP TABLE IF EXISTS `order_detail`;
+CREATE TABLE `order_detail`  (
+                                 `id` int NOT NULL AUTO_INCREMENT COMMENT '主键',
+                                 `order_id` int NOT NULL COMMENT '订单id',
+                                 `goods_id` int NULL DEFAULT NULL COMMENT '商品id',
+                                 `num` int NOT NULL DEFAULT 0 COMMENT '商品购买数量',
+                                 `amount` decimal(10, 2) NULL DEFAULT NULL COMMENT '总价',
+                                 PRIMARY KEY (`id`) USING BTREE,
+                                 INDEX `order_detail_orders_id_fk`(`order_id` ASC) USING BTREE,
+                                 INDEX `order_detail_goods_id_fk`(`goods_id` ASC) USING BTREE,
+                                 CONSTRAINT `order_detail_goods_id_fk` FOREIGN KEY (`goods_id`) REFERENCES `goods` (`id`) ON DELETE SET NULL ON UPDATE RESTRICT,
+                                 CONSTRAINT `order_detail_orders_id_fk` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '订单详情' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
 -- Table structure for orders
 -- ----------------------------
 DROP TABLE IF EXISTS `orders`;
 CREATE TABLE `orders`  (
-  `id` int NOT NULL AUTO_INCREMENT COMMENT '主键',
-  `number` int NOT NULL COMMENT '订单号',
-  `status` int NOT NULL COMMENT '订单状态 1待付款 2待接单 3已接单 4派送中 5已完成 6已取消 7退款',
-  `userid` int NOT NULL COMMENT '用户id',
-  `comment` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL COMMENT '订单备注',
-  `amount` decimal(10, 2) NULL DEFAULT NULL COMMENT '收款金额',
-  `order_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '下单时间',
-  PRIMARY KEY (`id`) USING BTREE,
-  UNIQUE INDEX `orders_number_uindex`(`number` ASC) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+                           `id` int NOT NULL AUTO_INCREMENT COMMENT '主键',
+                           `number` int NOT NULL COMMENT '订单号',
+                           `status` int NOT NULL COMMENT '订单状态 1待付款 2待接单 3已接单 4派送中 5已完成 6已取消 7退款',
+                           `userid` int NOT NULL COMMENT '用户id',
+                           `comment` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL COMMENT '订单备注',
+                           `amount` decimal(10, 2) NULL DEFAULT NULL COMMENT '收款金额',
+                           `order_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '下单时间',
+                           PRIMARY KEY (`id`) USING BTREE,
+                           UNIQUE INDEX `orders_number_uindex`(`number` ASC) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Table structure for users
 -- ----------------------------
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users`  (
-  `id` int NOT NULL AUTO_INCREMENT COMMENT '主键',
-  `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '用户名',
-  `nickname` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '昵称',
-  `hash` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '密码的哈希值',
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
-  PRIMARY KEY (`id`) USING BTREE,
-  UNIQUE INDEX `user_name`(`username` ASC) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+                          `id` int NOT NULL AUTO_INCREMENT COMMENT '主键',
+                          `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '用户名',
+                          `nickname` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '昵称',
+                          `hash` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '密码的哈希值',
+                          `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+                          `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+                          PRIMARY KEY (`id`) USING BTREE,
+                          UNIQUE INDEX `user_name`(`username` ASC) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
 
 SET FOREIGN_KEY_CHECKS = 1;

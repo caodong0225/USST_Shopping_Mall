cd3bf21eecb9a95b64a5acb8d11f192c8ea6caeb
jyzxc <867137498@qq.com>
2024-11-18 12:19:33 +0800

    feat: add payment bottom

--- android/app/src/main/java/com/caodong0225/shopping/ui/home/GoodsAdapter.kt
+++ android/app/src/main/java/com/caodong0225/shopping/ui/home/GoodsAdapter.kt
@@ -1,6 +1,7 @@
 package com.caodong0225.shopping.ui.home
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.caodong0225.shopping.databinding.GoodsItemBinding
@@ -37,6 +38,35 @@ class GoodsAdapter(private val goodsList: List<GoodsInfo>) : RecyclerView.Adapte
                 .load(goods.goods.logo) // 图片 URL
                 .into(binding.goodsImage) // 加载到 ImageView
             // Bind other properties to your views (like goods description, image, etc.)
+            // 初始化商品数量
+            var goodsNum = 0 // 假设你有一个 `number` 表示数量
+
+            // 显示初始值
+            binding.goodsNum.text = goodsNum.toString()
+            updateGoodsNumVisibility(goodsNum)
+
+            // 增加数量按钮
+            binding.goodsAdd.setOnClickListener {
+                if (goodsNum < goods.number) {
+                    goodsNum++
+                    binding.goodsNum.text = goodsNum.toString()
+                    updateGoodsNumVisibility(goodsNum)
+                }
+            }
+
+            // 减少数量按钮
+            binding.goodsMinus.setOnClickListener {
+                if (goodsNum > 0) {
+                    goodsNum--
+                    binding.goodsNum.text = goodsNum.toString()
+                    updateGoodsNumVisibility(goodsNum)
+                }
+            }
+        }
+        private fun updateGoodsNumVisibility(goodsNum: Int) {
+            // 如果数量为 0，隐藏数量显示；否则显示
+            binding.goodsNum.text = if (goodsNum == 0) "" else goodsNum.toString()
         }
     }
+
 }
